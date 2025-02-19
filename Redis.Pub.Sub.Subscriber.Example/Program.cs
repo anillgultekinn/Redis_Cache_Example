8cdfeec163bbe1d32352c9112b553fcfa4522530
Anıl Gültekin <anil.gultekin@goturc.com>
2025-02-19 10:22:20 +0300

    redis message broker olarakta kullanılabilir

---
+++ Redis.Pub.Sub.Subscriber.Example/Program.cs
@@ -0,0 +1,12 @@
+﻿using StackExchange.Redis;
+
+ConnectionMultiplexer connection = await ConnectionMultiplexer.ConnectAsync("localhost:3000");
+
+ISubscriber subscriber = connection.GetSubscriber();
+
+await subscriber.SubscribeAsync("mychannel.*", (channel, message) =>
+{
+    Console.WriteLine(channel);
+});
+
+Console.Read();
